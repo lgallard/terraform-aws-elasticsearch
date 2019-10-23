62b43ad11eca16ca0bffb2a283732fd4bc04a073
Luis M. Gallardo D <lgallard@gmail.com>
2019-10-23 19:07:42 -0300

    Add arguments and dynamic blocks for:   * advanced_options   * ebs_options   * encrypt_at_resti   * node_to_node_encryption   * cluster_config   * snapshot_options   * vpc_options   * log_publishing_options   * elasticsearch_version

--- main.tf
+++ main.tf
@@ -1,30 +1,15 @@
 resource "aws_elasticsearch_domain" "es_domain" {
-  domain_name           = var.domain_name
+  # Domain name
+  domain_name = var.domain_name
+
+  # ElasticSeach version
   elasticsearch_version = var.es_version
 
-  # cluster_config
-  dynamic "cluster_config" {
-    for_each = local.cluster_configs
-    content {
-      instance_type            = lookup(cluster_configs.value, "instance_type", var.cluster_instance_type)
-      instance_count           = lookup(cluster_configs.value, "instance_count", var.cluster_instance_count)
-      dedicated_master_enabled = lookup(cluster_configs.value, "dedicated_master_enabled", var.cluster_dedicated_master_enabled)
-      dedicated_master_type    = lookup(cluster_configs.value, "dedicated_master_type", var.cluster_dedicated_master_type)
-      dedicated_master_count   = lookup(cluster_configs.value, "dedicated_master_count", var.cluster_dedicated_master_count)
-      zone_awareness_enabled   = lookup(cluster_configs.value, "zone_awareness_enabled ", var.cluster_zone_awareness_enabled)
-
-      # zone_awareness_config
-      dynamic "zone_awareness_config" {
-        for_each = lookup(cluster_configs.value, "zone_awareness_enabled ", var.cluster_zone_awareness_enabled) == false || length(lookup(cluster_configs.value, "zone_awareness_config")) == 0 || lookup(cluster_configs.value, "zone_awareness_config") == null ? [] : [lookup(cluster_configs.value, "zone_awareness_config", {})]
-        content {
-          availability_zone_count = lookup(zone_awareness_config.value, "availability_zone_count", var.cluster_availability_zone_count)
-        }
-      }
-
-    }
-  }
-
+  # access_policies
   access_policies = var.access_policies
+
+  # advanced_options
+  advanced_options = var.advanced_options == null ? {} : var.advanced_options
 
   # ebs_options
   dynamic "ebs_options" {
@@ -34,8 +19,82 @@ resource "aws_elasticsearch_domain" "es_domain" {
       volume_type = lookup(ebs_options.value, "volume_type ", var.ebs_options_volume_type)
       volume_size = lookup(ebs_options.value, "volume_size", var.ebs_options_volume_size)
       iops        = lookup(ebs_options.value, "iops", var.ebs_options_iops)
+    }
+  }
 
+  # encrypt_at_rest
+  dynamic "encrypt_at_rest" {
+    for_each = local.encrypt_at_rest
+    content {
+      enabled    = lookup(encrypt_at_rest.value, "enabled ", var.encrypt_at_rest_enabled)
+      kms_key_id = lookup(encrypt_at_rest.value, "kms_key_id ", var.encrypt_at_rest_kms_key_id)
+    }
+  }
 
+  # node_to_node_encryption
+  dynamic "node_to_node_encryption" {
+    for_each = local.node_to_node_encryption
+    content {
+      enabled = lookup(node_to_node_encryption.value, "enabled ", var.node_to_node_encryption)
+    }
+  }
+
+  # cluster_config
+  dynamic "cluster_config" {
+    for_each = local.cluster_config
+    content {
+      instance_type            = lookup(cluster_config.value, "instance_type", var.cluster_instance_type)
+      instance_count           = lookup(cluster_config.value, "instance_count", var.cluster_instance_count)
+      dedicated_master_enabled = lookup(cluster_config.value, "dedicated_master_enabled", var.cluster_dedicated_master_enabled)
+      dedicated_master_type    = lookup(cluster_config.value, "dedicated_master_type", var.cluster_dedicated_master_type)
+      dedicated_master_count   = lookup(cluster_config.value, "dedicated_master_count", var.cluster_dedicated_master_count)
+      zone_awareness_enabled   = lookup(cluster_config.value, "zone_awareness_enabled ", var.cluster_zone_awareness_enabled)
+
+      # zone_awareness_config
+      dynamic "zone_awareness_config" {
+        for_each = lookup(cluster_config.value, "zone_awareness_enabled ", var.cluster_zone_awareness_enabled) == false || length(lookup(cluster_config.value, "zone_awareness_config")) == 0 || lookup(cluster_config.value, "zone_awareness_config") == null ? [] : [lookup(cluster_configs.value, "zone_awareness_config", {})]
+        content {
+          availability_zone_count = lookup(zone_awareness_config.value, "availability_zone_count", var.cluster_availability_zone_count)
+        }
+      }
+    }
+  }
+
+  # snapshot_options
+  dynamic "snapshot_options" {
+    for_each = local.snapshot_options
+    content {
+      automated_snapshot_start_hour = lookup(snapshot_options.value, "automated_snapshot_start_hour", var.snapshot_options_automated_snapshot_start_hour)
+    }
+  }
+
+  # vpc_options
+  dynamic "vpc_options" {
+    for_each = local.vpc_options
+    content {
+      security_group_ids = lookup(vpc_options.value, "security_group_ids", var.vpc_options_security_group_ids)
+      subnet_ids         = lookup(vpc_options.value, "subnet_ids", var.vpc_options_subnet_ids)
+    }
+  }
+
+  # log_publishing_options
+  dynamic "log_publishing_options" {
+    for_each = local.log_publishing_options
+    content {
+      log_type                 = lookup(log_publishing_options.value, "log_type", var.log_publishing_options_log_type)
+      cloudwatch_log_group_arn = lookup(log_publishing_options.value, "cloudwatch_log_group_arn", var.log_publishing_options_cloudwatch_log_group_arn)
+      enabled                  = lookup(log_publishing_options.value, "enabled", var.log_publishing_options_enabled)
+    }
+  }
+
+  # cognito_options
+  dynamic "cognito_options" {
+    for_each = local.cognito_options
+    content {
+      enabled          = lookup(cognito_options.value, "enabled", var.cognito_options_enabled)
+      user_pool_id     = lookup(cognito_options.value, "user_pool_id", var.cognito_options_user_pool_id)
+      identity_pool_id = lookup(cognito_options.value, "identity_pool_id ", var.cognito_options_identity_pool_id)
+      role_arn         = lookup(cognito_options.value, "role_arn", var.cognito_options_role_arn)
     }
   }
 
@@ -43,36 +102,88 @@ resource "aws_elasticsearch_domain" "es_domain" {
 
 locals {
 
+  # ebs_options
+  # If no ebs_options is provided, build a ebs_options using the default values
+  ebs_option_default = var.ebs_options != null ? var.ebs_options : {
+    ebs_enabled = var.ebs_enabled
+    volume_type = var.ebs_options_volume_type
+    volume_size = var.ebs_options_volume_size
+    iops        = var.ebs_options_iops
+  }
+
+  ebs_options = lookup(local.ebs_option_default, "ebs_enabled", false) == false ? [] : [ebs_option_default]
+
+  # encrypt_at_rest
+  # If no encrypt_at_rest list is provided, build a encrypt_at_rest using the default values
+  encrypt_at_rest_default = var.encrypt_at_rest != null ? var.encrypt_at_rest : {
+    enabled    = var.encrypt_at_rest_enabled
+    kms_key_id = var.encrypt_at_rest_kms_key_id
+  }
+
+  encrypt_at_rest = lookup(local.encrypt_at_rest_default, "enabled", false) == false ? [] : [local.encrypt_at_rest_default]
+
+  # node_to_node_encryption
+  # If no node_to_node_encryption list is provided, build a node_to_node_encryption using the default values
+  node_to_node_encryption_default = var.node_to_node_encryption != null ? var.node_to_node_encryption : {
+    enabled = var.node_to_node_encryption_enabled
+  }
+
+  node_to_node_encryption = lookup(local.node_to_node_encryption_default, "enabled", false) == false ? [] : [local.node_to_node_encryption_default]
+
+  # cluster_config
   # If no cluster_config list is provided, build a cluster_config using the default values
-  cluster_config = var.cluster_config == null ? [] : [
-    {
+  cluster_config_default = var.cluster_config != null ? var.cluster_config : {
+    instance_type            = var.cluster_instance_type
+    instance_count           = var.cluster_instance_count
+    dedicated_master_enabled = var.cluster_dedicated_master_enabled
+    dedicated_master_type    = var.cluster_dedicated_master_type
+    dedicated_master_count   = var.cluster_dedicated_master_count
 
-      instance_type            = var.cluster_instance_type
-      instance_count           = var.cluster_instance_count
-      dedicated_master_enabled = var.cluster_dedicated_master_enabled
-      dedicated_master_type    = var.cluster_dedicated_master_type
-      dedicated_master_count   = var.cluster_dedicated_master_count
+    # cluster_availability_zone_count valid values: 2 or 3.
+    zone_awareness_config = ! contains([2, 3], var.cluster_availability_zone_count) ? {} : {
+      availability_zone_count = var.cluster_availability_zone_count
+      zone_awareness_enabled  = true
+    }
+  }
 
-      # cluster_availability_zone_count valid values: 2 or 3.
-      zone_awareness_config = ! contains([2, 3], var.cluster_availability_zone_count) ? {} : {
-        availability_zone_count = var.cluster_availability_zone_count
-        zone_awareness_enabled  = true
-      }
+  cluster_config = [cluster_config_default]
 
-  }]
+  # snapshot_options
+  # If no snapshot_options list is provided, build a snapshot_options using the default values
+  snapshot_options_default = var.snapshot_options != null ? var.snapshot_options : {
+    automated_snapshot_start_hour = var.snapshot_options_automated_snapshot_start_hour
+  }
 
-  cluster_configs = concat(local.cluster_config, var.cluster_config)
+  snapshot_options = [local.snapshot_options_default]
 
-  # If no ebs_options list is provided, build a ebs_options using the default values
-  ebs_option = var.ebs_options == null || var.ebs_enabled == false ? [] : [
-    {
+  # vpc_options
+  # If no vpc_options list is provided, build a vpc_options using the default values
+  vpc_options_default = var.vpc_options != null ? var.vpc_options : {
+    security_group_ids = var.vpc_options_security_group_ids
+    subnet_ids         = var.vpc_options_subnet_ids
+  }
 
-      ebs_enabled = var.ebs_enabled
-      volume_type = var.ebs_options_volume_type
-      volume_size = var.ebs_options_volume_size
-      iops        = var.ebs_options_iops
-  }]
+  vpc_options = lookup(local.vpc_options_default, "subnet_ids", null) == null ? [] : [local.vpc_options_default]
 
-  ebs_options = concat(local.ebs_option, var.ebs_options)
+  # log_publishing_options
+  # If no log_publishing_options list is provided, build a log_publishing_options using the default values
+  log_publishing_options_default = var.log_publishing_options != null ? var.log_publishing_options : {
+    log_type                 = var.log_publishing_options_log_type
+    cloudwatch_log_group_arn = var.log_publishing_options_cloudwatch_log_group_arn
+    enabled                  = var.log_publishing_options_enabled
+  }
+
+  log_publishing_options = lookup(local.log_publishing_options_default, "enabled", null) == null ? [] : [local.log_publishing_options_default]
+
+  # cognito_options
+  # If no cognito_options list is provided, build a cognito_options using the default values
+  cognito_options_default = var.cognito_options != null ? var.cognito_options : {
+    enabled          = var.cognito_options_enabled
+    user_pool_id     = var.cognito_options_user_pool_id
+    identity_pool_id = var.cognito_options_identity_pool_id
+    role_arn         = var.cognito_options_role_arn
+  }
+
+  cognito_options = lookup(local.cognito_options_default, "enabled", null) == null ? [] : [local.cognito_options_default]
 
 }
--- variables.tf
+++ variables.tf
@@ -12,7 +12,89 @@ variable "es_version" {
   defualt     = "7.1"
 }
 
+variable "access_policies" {
+  description = "IAM policy document specifying the access policies for the domain"
+  type        = string
+  default     = ""
+}
+
+# Advanced options
+variable "advanced_options" {
+  description = "Key-value string pairs to specify advanced configuration options. Note that the values for these configuration options must be strings (wrapped in quotes) or they may be wrong and cause a perpetual diff, causing Terraform to want to recreate your Elasticsearch domain on every apply"
+  type        = map(string)
+  default     = {}
+}
+
+# ebs_options
+variable "ebs_options" {
+  description = "EBS related options, may be required based on chosen instance size"
+  type        = map
+  default     = {}
+}
+
+variable "ebs_enabled" {
+  description = "Whether EBS volumes are attached to data nodes in the domain"
+  type        = bool
+  default     = true
+}
+
+variable "ebs_options_volume_type" {
+  description = "The type of EBS volumes attached to data nodes"
+  type        = string
+  default     = "gp2"
+}
+
+variable "ebs_options_volume_size" {
+  description = "The size of EBS volumes attached to data nodes (in GB). Required if ebs_enabled is set to true"
+  type        = number
+  default     = 10
+}
+
+variable "ebs_options_iops" {
+  description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
+  type        = number
+  default     = 0
+}
+
+# encrypt_at_rest
+variable "encrypt_at_rest" {
+  description = "Encrypt at rest options. Only available for certain instance types"
+  type        = map
+  default     = {}
+}
+
+variable "encrypt_at_rest_enabled" {
+  description = "Whether to enable encryption at rest"
+  type        = bool
+  default     = true
+}
+
+variable "encrypt_at_rest_kms_key_id" {
+  description = "The KMS key id to encrypt the Elasticsearch domain with. If not specified then it defaults to using the aws/es service KMS key"
+  type        = string
+  default     = "aws/es"
+}
+
+# node_to_node_encryption
+variable "node_to_node_encryption" {
+  description = "Node-to-node encryption options"
+  type        = map
+  default     = {}
+}
+
+variable "node_to_node_encryption_enabled" {
+  description = "Whether to enable node-to-node encryption"
+  type        = bool
+  default     = true
+}
+
 # cluster_config 
+variable "cluster_config" {
+  description = "Cluster configuration of the domain"
+  type        = map
+  default     = {}
+}
+
 variable "cluster_instance_type" {
   description = "Instance type of data nodes in the cluster"
   type        = string
@@ -49,29 +131,84 @@ variable "cluster_availability_zone_count" {
   default     = 3
 }
 
-variable "access_policies" {
-  description = "IAM policy document specifying the access policies for the domain"
+# snapshot_options
+variable "snapshot_options" {
+  description = "Snapshot related options"
+  type        = map
+  default     = {}
+}
+
+variable "snapshot_options_automated_snapshot_start_hour" {
+  description = "Hour during which the service takes an automated daily snapshot of the indices in the domain"
+  type        = number
+  default     = 0
+}
+
+# vpc_options
+variable "vpc_options" {
+  description = "VPC related options, see below. Adding or removing this configuration forces a new resource"
+  type        = map
+  default     = {}
+}
+
+variable "vpc_options_security_group_ids" {
+  description = "List of VPC Security Group IDs to be applied to the Elasticsearch domain endpoints. If omitted, the default Security Group for the VPC will be used"
+  type        = list
+  default     = []
+}
+
+variable "vpc_options_subnet_ids" {
+  description = "List of VPC Subnet IDs for the Elasticsearch domain endpoints to be created in"
+  type        = list
+  default     = []
+}
+
+# log_publishing_options 
+variable "log_publishing_options" {
+  description = "Options for publishing slow logs to CloudWatch Logs"
+  type        = map
+  default     = {}
+}
+
+variable "log_publishing_options_log_type" {
+  description = "A type of Elasticsearch log. Valid values: INDEX_SLOW_LOGS, SEARCH_SLOW_LOGS, ES_APPLICATION_LOGS"
+  type        = string
+  default     = "INDEX_SLOW_LOGS"
+}
+
+variable "log_publishing_options_cloudwatch_log_group_arn" {
+  description = "iARN of the Cloudwatch log group to which log needs to be published"
+  type        = string
   default     = ""
 }
 
-
-# ebs_options
-variable "ebs_enabled" {
-  description = "Whether EBS volumes are attached to data nodes in the domain"
+variable "log_publishing_options_enabled" {
+  description = "Specifies whether given log publishing option is enabled or not"
+  type        = bool
   default     = true
 }
 
-variable "ebs_options_volume_type" {
-  description = "The type of EBS volumes attached to data nodes"
-  default     = "gp2"
+# cognito_options  
+variable "cognito_options" {
+  description = "Options for Amazon Cognito Authentication for Kibana"
+  type        = map
+  default     = {}
 }
 
-variable "ebs_options_volume_size" {
-  description = "The size of EBS volumes attached to data nodes (in GB). Required if ebs_enabled is set to true"
-  default     = 10
+variable "cognito_options_enabled" {
+  description = "Specifies whether Amazon Cognito authentication with Kibana is enabled or not"
+  type        = bool
+  default     = true
 }
 
-variable "ebs_options_iops" {
-  description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
-  default     = 0
+variable "cognito_options_user_pool_id " {
+  description = "ID of the Cognito User Pool to use"
+  type        = string
+  default     = ""
+}
+
+variable "cognito_options_identity_pool_id" {
+  description = "ID of the Cognito Identity Pool to use"
+  type        = string
+  default     = ""
 }
