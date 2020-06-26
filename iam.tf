2da8c45043598873d02947864f7cbaf3cfb568ce
Luis M. Gallardo D <lgallard@gmail.com>
2020-06-26 14:58:40 -0300

    Add Service Link Role flag

--- iam.tf
+++ iam.tf
@@ -29,6 +29,7 @@ CONFIG
 
 # Service-linked role to give Amazon ES permissions to access your VPC
 resource "aws_iam_service_linked_role" "es" {
+  count            = var.create_service_link_role == true ? 1 : 0
   aws_service_name = "es.amazonaws.com"
   description      = "Service-linked role to give Amazon ES permissions to access your VPC"
 }
