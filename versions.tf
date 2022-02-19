4267d338f6737baa67bc67981ee101294fbd5ebf
Luis M. Gallardo D <lgallard@gmail.com>
2022-02-19 11:20:20 -0300

    Update required provider syntax

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.12.9"
 
   required_providers {
-    aws = ">= 3.35.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.35.0"
+    }
   }
 }
