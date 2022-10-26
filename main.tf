0ad6d953cdd289a747eda0e9232ac15a3a89b51c
Roger Fava (Group) <roger.fava@global.ntt>
2022-10-26 17:56:13 +0200

    Debugging

--- main.tf
+++ main.tf
@@ -293,7 +293,8 @@ locals {
 
   # Create subblock master_user_options
   # duration_value = var.auto_tune_options_desired_state == "ENABLED" && var.auto_tune_options_start_at != null ? var.auto_tune_options_duration_value : null
-  duration_value = 3
+  duration_value = var.auto_tune_options_duration_value
+  # duration_value = 3
   # start_at = var.auto_tune_options_desired_state == "ENABLED" ? var.auto_tune_options_start_at : null
   start_at = "2022-10-25T04:00:00.00Z"
   # cron_expression_for_recurrence = var.auto_tune_options_desired_state == "ENABLED" && var.auto_tune_options_start_at != null ? var.auto_tune_options_cron_expression_for_recurrence : null
