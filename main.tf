df83b92becb96f1649f0a2872757a40b069b285f
Roger Fava (Group) <roger.fava@global.ntt>
2022-10-26 12:41:27 +0200

    Added auto-tune options

--- main.tf
+++ main.tf
@@ -293,7 +293,7 @@ locals {
 
   # Create subblock master_user_options
   duration_value = var.auto_tune_options_desired_state == "ENABLED" && var.auto_tune_options_start_at != null ? var.auto_tune_options_duration_value : null
-  start_at = var.auto_tune_options_desired_state == "ENABLED" ? auto_tune_options_start_at : null
+  start_at = var.auto_tune_options_desired_state == "ENABLED" ? var.auto_tune_options_start_at : null
   cron_expression_for_recurrence = var.auto_tune_options_desired_state == "ENABLED" && var.auto_tune_options_start_at != null ? var.auto_tune_options_cron_expression_for_recurrence : null
 
   duration = lookup(lookup(var.auto_tune_options, "maintenance_schedule"), "duration") != null ? lookup(lookup(var.auto_tune_options, "maintenance_schedule"), "duration") : {
