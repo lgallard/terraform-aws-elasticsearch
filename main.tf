f05d78c83cdf515991e24d901e1bfe9956d1fbac
Roger Fava (Group) <roger.fava@global.ntt>
2022-11-03 16:37:41 +0100

    Debugging

--- main.tf
+++ main.tf
@@ -223,8 +223,8 @@ locals {
   maintenance_schedule = lookup(var.auto_tune_options, "maintenance_schedule", null) != null ? lookup(var.auto_tune_options, "maintenance_schedule") : {
     start_at      = local.start_at
     duration = {
-      value = null
-      unit = null
+      value = 1
+      unit = "HOURS"
     }
     # duration     = local.duration
     cron_expression_for_recurrence = local.cron_expression_for_recurrence
